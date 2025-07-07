940a76306b91ff22d42e63d934df6052a113d9c8
hiihnunin <a57051579@gmail.com>
2025-07-07 11:45:06 +0900

    Create watches.js

---
+++ data/watches.js
@@ -0,0 +1,11 @@
+const watchData = {
+  "ROLEX": [
+    { name: "Submariner Date", img: "images/rolex1.jpg", inStock: true },
+    { name: "Day-Date 40", img: "images/rolex2.jpg", inStock: true },
+    { name: "GMT-Master II", img: "images/rolex3.jpg", inStock: false }
+  ],
+  "OMEGA": [
+    { name: "Seamaster Aqua Terra", img: "images/omega1.jpg", inStock: true },
+    { name: "Speedmaster Moonwatch", img: "images/omega2.jpg", inStock: false }
+  ]
+};
